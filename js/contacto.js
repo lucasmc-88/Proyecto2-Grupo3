f04ac9e16989e2a9e1e5ba688d715358e095693e
LeoJzDev <leo.jz.dev@gmail.com>
2022-12-16 17:39:44 -0300

    Modifique el nav e hice la pagina de contacto

---
+++ js/contacto.js
@@ -0,0 +1,49 @@
+let nombre = document.getElementById("nombre")
+let mensaje = document.getElementById("mensaje")
+let email = document.getElementById("email")
+
+email.addEventListener("blur", () => {
+    campoEmail(email)
+})
+nombre.addEventListener("blur", () => {
+    campoNombre(nombre)
+})
+
+mensaje.addEventListener("blur", () => {
+    campoMensaje(mensaje)
+})
+
+const campoNombre = (input) => {
+    if (input.value.trim().length > 0) {
+        input.className = "form-control is-valid";
+        return true;
+      } else {
+        input.className = "form-control is-invalid";
+        return false;
+      }
+}
+const campoMensaje = (input) => {
+    if(input.value.trim().length >= 10) {
+        input.className = "form-control is-valid";
+        return true;
+    }else{
+        input.className = "form-control is-invalid";
+        return false;
+    }
+}
+const campoEmail = (input) => {
+    let patron = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
+    if (patron.test(input.value)) {
+        input.className = "form-control is-valid";
+        return true;
+      } else {
+        input.className = "form-control is-invalid";
+        return false;
+      }
+}
+
+
+
+
+
+
