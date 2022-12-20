fd8e01ba8dd7c355fe5fba13012b0394accd1ae0
Lucas <lucasmcoronel@gmail.com>
2022-12-19 23:36:02 -0300

    se agrego un vinculo de carrusel de prueba

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+let listaJuegos = JSON.parse(localStorage.getItem("arrayJuegoKey")) || [];
+
+listaJuegos.map((item) => {crearColumna(item)});
+//listaProductos.forEach((item) => {crearColumna(item)});
+
+function crearColumna(juego) {
+  let grilla = document.getElementById('grilla');
+  grilla.innerHTML +=` <div class="col-12 col-md-4 col-lg-3 mb-3 mx-3 text-black">
+  <div class="card h-100">
+    <img src="${juego.publico}" class="card-img-top" alt="${juego.juego}">
+    <div class="card-body">
+      <h5 class="card-title">${juego.juego}</h5>
+      <p class="card-text">${juego.descripcion}</p>
+    </div>
+  </div>
+</div>`
+};
